dc6bed8b710ea75c45f175a47294d74f30e303f6
lorenascimentoo <lorenanascimento28@gmail.com>
2019-06-26 14:32:41 -0300

    listar cat

--- src/pages/Bulas/index.js
+++ src/pages/Bulas/index.js
@@ -1,30 +1,53 @@
 import React, { Component } from "react";
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Button } from 'reactstrap';
+import Header from '../../components/Header';
 
 import api from "../../services/api";
 
 export class ListBulas extends Component {
-    state = {
-        bulas: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            bulas: [],
+            bulaId: null
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    
+    handleClick(id) {
+        this.setState({ bulaId : id });
+        console.log('Clicado');
     }
 
     componentWillMount() {
         api.get('/bulas')
-            .then(dados => this.setState({bulas: dados.data}))
+            .then(dados => this.setState({ bulas: dados.data }))
             .catch(() => this.setState('erro ao recuperar dados'));
     }
-  
+
     render() {
         const { bulas } = this.state;
         console.log(bulas);
         return (
-            <ul>
-                {this.state.bulas.map(function(item,index){
-                    return <div key={item.id}>
-                            <h1>{item.nomeComercial}</h1>
-                            <h3>{item.fabricante}</h3>
-                        </div>
-                })}
-            </ul>
-        );
+            <div>
+                <Header title="SIBMED" />
+                <ListGroup>
+                    {this.state.bulas.map(function (item, index) {
+                        return (
+                            <button type="submit" key={item.id} >
+                                    <ListGroupItem  action>
+                                        <ListGroupItemHeading>{item.nomeComercial}</ListGroupItemHeading>
+                                        <ListGroupItemText>{item.fabricante}</ListGroupItemText>
+                                    </ListGroupItem>
+                            </button>
+                        )
+                    })}
+                </ListGroup>
+                <Button color="primary" size="lg" >Inserir</Button>{' '}
+                <Button color="secondary" size="lg" >Pesquisar</Button>
+
+            </div>
+        )
     }
 }
