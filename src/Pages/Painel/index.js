4ff8a073355de6cc4ce20a78201e17dbf214b6ed
lorenascimentoo <lorenanascimento28@gmail.com>
2019-06-30 15:25:16 -0300

    Painel de Bulas Medicas enviadas pelo usuario com opção de delecao de bula

--- src/Pages/Painel/index.js
+++ src/Pages/Painel/index.js
@@ -1,15 +1,81 @@
-import React, {Component} from 'react';
-import {Container } from "./styles";
+import React, { Component } from 'react';
+import api from '../../services/api';
+import GlobalStyle from '../../styles';
+import { Container, Content } from './styles';
+import { Table } from 'reactstrap';
+import { MdDelete } from "react-icons/md";
 
 
-export default class Painel extends Component{
-    render(){
-        return(
+export class Painel extends Component {
+    state = {
+        bulas: []
+    }
+
+    componentDidMount() {
+        api.get("/bulas/usuario")
+            .then(dados => this.setState({ bulas: dados.data }))
+            .catch(e => {
+                alert('Seu token está expirado, faça login novamente" :)');
+                this.props.history.push(`/login`);
+            });
+    }
+    
+    handleDelete(id) {
+        api.delete(`/bulas/${id}`)
+        .then(dados => {
+            alert(dados.statusText)
+            window.location.reload();
+        })
+            .catch(e => {
+                console.log(e)
+                //alert('Seu token está expirado, faça login novamente" :)');
+                //this.props.history.push(`/login`);
+            });
+    }
+
+    render() {
+        const { bulas } = this.state;
+        console.log(bulas);
+
+        return (
             <Container>
-            <div>
-               <h1>Gestão de Bulas</h1>
-            </div>
+                <Content>
+                    <div>
+                        <h2>Sibmed</h2>
+                    </div>
+
+                    <Table hover>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome Comercial</th>
+                                <th>Fabricante</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                bulas.map(bula => (
+                                    <tr key={bula.id}>
+                                        <th>{bula.id}</th>
+                                        <th>{bula.nomeComercial}</th>
+                                        <th>{bula.fabricante}</th>
+                                        <th>
+                                        <MdDelete size={24} color="#e57878" onClick={() => this.handleDelete(bula.id)}/>
+                                        </th>
+                                    </tr>
+                                )
+                                )
+                            }
+
+                        </tbody>
+                    </Table>
+                </Content>
+                <GlobalStyle />
             </Container>
-        )
+
+        );
     }
 }
+
+export default Painel;
--- src/Pages/Painel/styles.js
+++ src/Pages/Painel/styles.js
@@ -1,63 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  height: 100%;
   display: flex;
-  align-items: center;
   justify-content: center;
-  height: 100vh;
+  align-items: center;
 `;
 
-export const Form = styled.form`
-  width: 400px;
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 900px;
   background: #fff;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  img {
-    width: 100px;
-    margin: 10px 0 40px;
-  }
-  p {
-    color: #ff3333;
-    margin-bottom: 15px;
-    border: 1px solid #ff3333;
-    padding: 10px;
-    width: 100%;
-    text-align: center;
-  }
-  input {
-    flex: 1;
-    height: 46px;
-    margin-bottom: 15px;
-    padding: 0 20px;
-    color: #777;
-    font-size: 15px;
-    width: 100%;
-    border: 1px solid #ddd;
-    &::placeholder {
-      color: #999;
-    }
-  }
-  button {
-    color: #fff;
-    font-size: 16px;
-    background: #fc6963;
-    height: 56px;
-    border: 0;
-    border-radius: 5px;
-    width: 100%;
-  }
-  hr {
-    margin: 20px 0;
-    border: none;
-    border-bottom: 1px solid #cdcdcd;
-    width: 100%;
-  }
-  a {
-    font-size: 16;
-    font-weight: bold;
-    color: #999;
-    text-decoration: none;
-  }
+  margin: 30px;
+  padding: 30px;
 `;
